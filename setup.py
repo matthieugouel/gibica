b802bb77e687afaad0c8294bf76023c09076e0db
matthieu <matthieu.gouel@gmail.com>
2018-06-09 13:43:54 +0200

    Add back compatibility to pip <= 9.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 import uuid
 
 from setuptools import setup, find_packages
-from pip._internal.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 INSTALL_REQS = parse_requirements('requirements.txt', session=uuid.uuid1())
 REQS = [str(ir.req) for ir in INSTALL_REQS]
