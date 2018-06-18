404cc28d7f3a5f8cb69c657331d17d0ab6f7003d
matthieu <matthieu.gouel@gmail.com>
2018-06-18 22:04:29 +0200

    Add some tests with booleans

--- test/test_declaration.py
+++ test/test_declaration.py
@@ -43,6 +43,11 @@ def test_float_declaration(evaluate, input, expected):
 @pytest.mark.parametrize('input, expected', [
     ('bool a = true;', {'a': Bool(True)}),
     ('bool a = false;', {'a': Bool(False)}),
+    ('bool a = true == 1 < 2;', {'a': Bool(True)}),
+    ('bool a = true == 1 >= 2;', {'a': Bool(False)}),
+    ('bool a = true == true;', {'a': Bool(True)}),
+    ('bool a = false == false;', {'a': Bool(True)}),
+    ('bool a = true == false;', {'a': Bool(False)}),
 ])
 def test_bool_declaration(evaluate, input, expected):
     """Test `Bool` type variable declaration."""
@@ -113,6 +118,11 @@ def test_reassignment_of_immutable_variable(evaluate, input):
     'int a = false + 1;',
     'float a = true + 1.0;',
     'float a = false + 1.0;',
+    'bool a = false + false;',
+    'bool a = true < false;',
+    'bool a = true > false;',
+    'bool a = true <= false;',
+    'bool a = true >= false;',
 ])
 def test_invalid_operation_or_comparison(evaluate, input):
     """Test invalid operation or comparison of variables."""
